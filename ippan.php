ba41b407cbd3b98a857897227a9b53ff53dfee78
kei333 <46550291+kei333@users.noreply.github.com>
2019-02-04 23:15:46 +0900

    Add files via upload

---
+++ ippan.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+            <meta charset="UTF-8">
+            <title>一般ユーザ</title>
+    </head>
+    <body>
+        <h1>ホーム画面</h1>
+        <p>一般ユーザログイン完了</p>
+    </body>
+</html>
---
+++ kanrisya.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+            <meta charset="UTF-8">
+            <title>管理者</title>
+    </head>
+    <body>
+        <h1>ホーム画面</h1>
+        <p>管理者ログイン完了</p>
+    </body>
+</html>
