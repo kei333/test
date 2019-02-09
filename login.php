e0583886932ad0061aef6a4bd2e76469c8c0a791
kei333 <46550291+kei333@users.noreply.github.com>
2019-02-09 23:10:07 +0900

    Add files via upload

--- login.php
+++ login.php
@@ -1,15 +1,43 @@
 <?php
 // セッション開始
 session_start();
-
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=forever','root','' ); 
+    $id=$_POST["id"];
+    $password=$_POST["password"];
     foreach($dbh->query('SELECT * from user') as $row) {
+        $result = $dbh->query('SELECT * FROM user WHERE ID = ' .$id);
     }
     } catch (PDOException $e) {
         print "エラー!: " . $e->getMessage() . "<br/>";
         die();
     }
+?>
+
+ <?php
+ $errorMessage = '';
+// ログイ(ンボタンが押された場合
+if(isset($_POST['login'])){
+    // IDの入力チェック
+    if(empty($_POST["id"])){// emptyは値が空のとき
+        $errorMessage = 'IDが未入力です。';
+    }elseif(empty($_POST["password"])){
+        $errorMessage = 'パスワードが未入力です。';
+    } 
+}
+?>
+
+<?php
+//ログインボタンが押された場合
+if(isset($_POST["login"])){
+    if(($_POST['Admin_flg'])==1){
+        header('Location: http://localhost/forever/kanrisya.php');
+        exit;
+    }else{
+        header('Location: http://localhost/forever/ippan.php');
+        exit;
+    }
+}
 ?>
 <html>
     <head>
@@ -21,30 +49,8 @@ try {
         <form action="login.php" method="POST">
             <p>ID:<input type="text" name="id" placeholder="IDを入力"></p>
             <p>パスワード<input type="password" name="password" placeholder="パスワードを入力"></p>
+            <?php echo $errorMessage; ?>
             <input type="submit" id="login" name="login" value="ログイン">
         </form>
-            <?php
-            // ログイ(ンボタンが押された場合
-            if(isset($_POST['login'])){
-                // IDの入力チェック
-                if(empty($_POST["id"])){  // emptyは値が空のとき
-                    $errorMessage = 'IDが未入力です。';
-                }elseif(empty($_POST["password"])){
-                    $errorMessage = 'パスワードが未入力です。';
-                } 
-            }
-            ?>
-            <?php
-            //ログインボタンが押された場合
-            if(isset($_POST["login"])){
-                if(($_POST['Admin_flg'])==1){
-                    header('Location: http://localhost/forever/kanrisya.php');
-                    exit;
-                }else{
-                     header('Location: http://localhost/forever/ippan.php');
-                    exit;
-                }
-            }
-            ?>
     </body>
 </html>
